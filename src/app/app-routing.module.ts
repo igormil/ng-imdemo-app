028a89fb4df9aa12510f17347487d9406abcc480
Igor Milovanovic <igor.milovanovic.bp@nttdata.ro>
2020-09-30 11:09:44 +0200

    Inital commit - basic components and routes;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HomeComponent } from './home/home.component'
+import { AboutMeComponent } from './about-me/about-me.component'
+import { MyProjectsComponent } from './my-projects/my-projects.component'
+import { PersonalComponent } from './personal/personal.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'personal',
+    component: PersonalComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'about-me', component: AboutMeComponent},
+      {path: 'my-projects', component: MyProjectsComponent},
+    ]
+  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component:  PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
